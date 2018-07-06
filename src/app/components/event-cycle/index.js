bebc09e76c7f6183d6e980d8cd3c308880b541ce
Luke Anderson <luke.kansas@gmail.com>
2018-07-05 22:40:52 -0500

    starting cycle component

--- src/app/components/event-cycle/index.js
+++ src/app/components/event-cycle/index.js
@@ -1,7 +1,7 @@
 import BaseComponent from 'components/_util/base-component';
 import Component from 'components/_util/component';
 // import { audioEventBus } from 'services/EventBus';
-// import metronomeManager from 'services/metronome/metronomeManager';
+import metronomeManager from 'services/metronome/metronomeManager';
 
 const COMPONENT_NAME = 'event-cycle';
 const style = require(`./${COMPONENT_NAME}.css`);
@@ -9,20 +9,50 @@ const markup = require(`./${COMPONENT_NAME}.html`);
 
 // const metronome = metronomeManager.getMetronome();
 const domMap = {
-  // container: 'container',
-  // thresholdInput: 'tresholdInput',
-  // sendComboBox: 'sendComboBox',
-  // valueInput: 'valueInput'
+  cycleLength: 'cycleLength',
+  cycleElement: 'cycleElement'
 };
 
 class EventCycle extends BaseComponent {
   constructor(node) {
     super(style, markup, domMap);
+    this.cycleLength = 4;
   }
 
-  connectedCallback() {}
+  connectedCallback() {
+    this.dom.cycleLength.addEventListener('blur', this.handleCycleLengthChange.bind(this));
 
-  disconnectedCallback() {}
+    this.metronomeSchedulable = this.buildMetronomeSchedulable();
+    metronomeManager.getScheduler().register(this.metronomeSchedulable);
+  }
+
+  disconnectedCallback() {
+    metronomeManager.getScheduler().deregister(this.metronomeSchedulable);
+  }
+
+  handleCycleLengthChange(event) {
+    this.cycleLength = parseInt(event.target.value, 10);
+  }
+
+  buildMetronomeSchedulable() {
+    return {
+      processTick: (tickNumber, time) => {
+        if (tickNumber % this.cycleLength === 0) {
+          console.log('startCycle', tickNumber);
+        }
+      },
+      render: (tickNumber, lastTickNumber) => {
+        if (tickNumber % this.cycleLength === 0) {
+          this.dom.cycleElement.classList.add('cycle-element--active');
+        }
+        else {
+          this.dom.cycleElement.classList.remove('cycle-element--active');
+        }
+      },
+      start: () => {},
+      stop: () => {}
+    };
+  }
 }
 
 export default new Component(COMPONENT_NAME, EventCycle);
