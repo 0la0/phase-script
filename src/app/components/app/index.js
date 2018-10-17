0813bf6f256858032f0facb95c69ec97e43860ef
Luke Anderson <luke.kansas@gmail.com>
2018-10-16 23:30:04 -0500

    more initialization cleanup

--- src/app/components/app/index.js
+++ src/app/components/app/index.js
@@ -2,15 +2,12 @@ import BaseComponent from 'components/_util/base-component';
 import Component from 'components/_util/component';
 
 const COMPONENT_NAME = 'app-entry';
-const style = require(`./${COMPONENT_NAME}.css`);
 const markup = require(`./${COMPONENT_NAME}.html`);
 
 class App extends BaseComponent {
-
   constructor() {
-    super(style, markup);
+    super('', markup);
   }
-
 }
 
 export default new Component(COMPONENT_NAME, App);
--- src/main.js
+++ src/main.js
@@ -3,27 +3,8 @@ import App from 'components/app';
 import { loadSamples } from 'services/audio/sampleBank';
 import { initListeners } from 'services/EventBus/listeners';
 
-function applyGlobalStyles() {
-  const globalStyles = {
-    width: '100%',
-    height: '100%',
-    position: 'relative',
-    margin: 0,
-    padding: 0,
-    'background-color': '#333333',
-    color: '#DDDDDD',
-    'font-family': 'helvetica, arial, sans-serif',
-    'font-size': '12px'
-  };
-  Object.keys(globalStyles).forEach((key) => {
-    document.body.style.setProperty(key, globalStyles[key]);
-    document.documentElement.style.setProperty(key, globalStyles[key]);
-  });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   loadSamples();
   initListeners();
-  applyGlobalStyles();
   document.body.appendChild(new App.element());
 });
