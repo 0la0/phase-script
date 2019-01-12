bd95e08f2ccd907fa8bd1b53a2006941c5eaa30e
Luke Anderson <luke.kansas@gmail.com>
2019-01-11 22:04:50 -0600

    pattern handler cleanup

--- src/app/services/EventCycle/Pattern/DegradeHandler.js
+++ src/app/services/EventCycle/Pattern/DegradeHandler.js
@@ -3,10 +3,6 @@ export function degradeHandler(threshold) {
     const transform = {
       predicate: () => true,
       transform: (_pattern) => {
-        // const transformedCycle = _pattern.getRelativeCycle().map((cycleElement) => {
-        //   const time = 1 - cycleElement.getTime();
-        //   return cycleElement.setTime(time);
-        // }).reverse();
         const transformedCycle = _pattern.getRelativeCycle()
           .filter(() => Math.random() < threshold);
         return _pattern.setRelativeCycle(transformedCycle);
--- src/app/services/EventCycle/Pattern/PatternHandler.js
+++ src/app/services/EventCycle/Pattern/PatternHandler.js
@@ -1,67 +1,6 @@
 import parseCycle from 'services/EventCycle/Parser';
 import { getRelativeCycle } from 'services/EventCycle/Evaluator';
 
-class Counter {
-  constructor(numTicks) {
-    this.numTicks = numTicks;
-    this.cnt = 0;
-  }
-
-  tick() {
-    return this.cnt++ === 0;
-  }
-
-  isDone() {
-    return this.cnt >= this.numTicks;
-  }
-
-  getNumTicks() {
-    return this.numTicks;
-  }
-
-  setNumTicks(numTicks) {
-    this.numTicks = numTicks;
-  }
-
-  reset() {
-    this.cnt = 0;
-  }
-}
-
-class EveryHandler {
-  constructor(iteration, pattern) {
-    this.iteration = iteration;
-    this.pattern = pattern;
-  }
-
-  requestPattern(cnt) {
-    if (cnt % this.iteration !== 0) {
-      return false;
-    }
-    return this.pattern.relativeCycle;
-  }
-}
-
-class CycleHandler {
-  constructor(patternString) {
-    this.pattern = parseCycle(patternString);
-    this.relativeCycle = this.pattern.ok ?
-      getRelativeCycle(this.pattern.content, 0, 1) : [];
-  }
-
-  getRelativeCycle() {
-    return this.relativeCycle;
-  }
-
-  setRelativeCycle(relativeCycle) {
-    this.relativeCycle = relativeCycle;
-  }
-
-  isValid() {
-    return this.pattern.ok;
-  }
-}
-
 export class Pattern {
   constructor(relativeCycle, numTicks, cnt) {
     this.relativeCycle = relativeCycle;
@@ -94,23 +33,18 @@ export class Pattern {
 
 export default class PatternHandler {
   constructor(patternString, numTicks) {
-    this.cycleHandlers = [ new CycleHandler(patternString), ];
-    this.counter = new Counter(numTicks || 16);
+    this.patternString = patternString;
+    this.pattern = parseCycle(patternString);
+    this.relativeCycle = this.pattern.ok ? getRelativeCycle(this.pattern.content, 0, 1) : [];
+    this.numTicks = numTicks || 16;
     this.cnt = 0;
     this.transforms = [];
   }
 
-  getRelativeCycle() {
-    return {
-      cycle: this.cycleHandlers[0].getRelativeCycle(),
-      updateCycle: updatedCycle => this.cycleHandlers[0].setRelativeCycle(updatedCycle),
-    };
-  }
-
   tick() {
     const cnt = this.cnt++;
-    const relativeCycle = this.getActiveCycle().map(ele => ele.clone());
-    const pattern = new Pattern(relativeCycle, this.counter.getNumTicks(), cnt);
+    const clonedCycle = this.relativeCycle.map(ele => ele.clone());
+    const pattern = new Pattern(clonedCycle, this.numTicks, cnt);
 
     return this.transforms.reduce((pattern, xform) => {
       const { transform, predicate } = xform;
@@ -121,42 +55,16 @@ export default class PatternHandler {
     }, pattern);
   }
 
-  getActiveCycle() {
-    return this.cycleHandlers[0].getRelativeCycle();
-  }
-
   pushToTransformStack(transform) {
     this.transforms.push(transform);
     return this;
   }
 
-  getNumTicks() {
-    return this.counter.getNumTicks();
-  }
-
-  isDone() {
-    return this.counter.isDone();
-  }
-
-  reset() {
-    this.counter.reset();
-  }
-
-  addEveryHandler(iteration, pattern) {
-    this.everyHandler = new EveryHandler(iteration, pattern);
-  }
-
-  setNumTicks(numTicks) {
-    this.counter.setNumTicks(numTicks);
-  }
-
   isValid() {
-    return this.cycleHandlers.every(handler => handler.isValid());
+    return this.pattern.ok;
   }
 
   clone() {
-    const instance = new PatternHandler('');
-    instance.cycleHandlers = this.cycleHandlers;
-    return instance;
+    return new PatternHandler(this.patternString);
   }
 }
